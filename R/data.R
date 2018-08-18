da8db2c2bb7fd9c471c55b721db8b834c66c75c8
Stefano Coretta <stefanoc.social@me.com>
2018-08-19 01:02:16 +0200

    add documentations

--- R/data.R
+++ R/data.R
@@ -96,7 +96,7 @@
 #' A dataset with displacement and velocity of tongue tip, dorsum and, root
 #' during the production of vowels in Italian and Polish.
 #'
-#' @format A data frame with 7496 observations and 64 variables:
+#' @format A tibble with 7496 observations and 64 variables:
 #' \describe{
 #'   \item{\code{speaker}}{speaker's ID}
 #'   \item{\code{seconds}}{(s) time within audio file}
@@ -164,3 +164,144 @@
 #'   \item{\code{c2_place}}{place of C2 (\code{coronal}, \code{velar})}
 #'}
 "kinematics_series"
+
+#' Articulator displacement and positions at defined time points in Italian and Polish
+#'
+#' A dataset with displacement and velocity of tongue tip, dorsum, and root at
+#' specified time points during the production of vowels and consonants in
+#' Italian and Polish.
+#'
+#' @format A tibble with 7598 observations and 27 variables:
+#' \describe{
+#'   \item{\code{speaker}}{speaker's ID}
+#'   \item{\code{seconds}}{(s) time within audio file}
+#'   \item{\code{rec_date}}{date and time of recording}
+#'   \item{\code{prompt}}{sentence stimulus}
+#'   \item{\code{label}}{\code{AAA} annotation label}
+#'   \item{\code{TT_displacement_sm}}{smoothed Tongue Tip displacement}
+#'   \item{\code{TT_velocity}}{Tongue Tip velocity}
+#'   \item{\code{TT_velocity_abs}}{absolute Tongue Tip velocity}
+#'   \item{\code{TD_displacement_sm}}{smoothed Tongue Dorsum displacement}
+#'   \item{\code{TD_velocity}}{Tongue Dorsum velocity}
+#'   \item{\code{TD_velocity_abs}}{absolute Tongue Dorsum velocity}
+#'   \item{\code{TR_displacement_sm}}{smoothed Tongue Root displacement}
+#'   \item{\code{TR_velocity}}{Tongue Root velocity}
+#'   \item{\code{TR_velocity_abs}}{Absolute Tongue Root velocity}
+#'   \item{\code{word}}{word stimulus}
+#'   \item{\code{language}}{speaker's native language (\code{Italian}, \code{Polish})}
+#'   \item{\code{sex}}{speaker's sex (\code{f}, \code{m})}
+#'   \item{\code{item}}{word ID number}
+#'   \item{\code{ipa}}{IPA transcription of the word}
+#'   \item{\code{c1}}{first consonant (C1)}
+#'   \item{\code{c1_phonation}}{voicing of C1 (\code{voiceless}, \code{voiced})}
+#'   \item{\code{vowel}}{V1 and V2 (\code{a}, \code{o}, \code{u})}
+#'   \item{\code{anteropost}}{backness of the vowel (\code{back}, \code{central})}
+#'   \item{\code{height}}{height of the vowel (\code{high}, \code{mid}, \code{low})}
+#'   \item{\code{c2}}{second consonant (C2)}
+#'   \item{\code{c2_phonation}}{voicing of C2 (\code{voiceless} or \code{voiced})}
+#'   \item{\code{c2_place}}{place of C2 (\code{coronal}, \code{velar})}
+#'}
+"kinematics"
+
+#' Tongue contours in Italian and Polish
+#'
+#' Coordinates of tongue contour splines of Italian and Polish at defined time points.
+#'
+#' @format A tibble with 319116 rows and 30 variables:
+#' \describe{
+#'   \item{\code{speaker}}{speaker's ID}
+#'   \item{\code{seconds}}{(s) time within audio file}
+#'   \item{\code{rec_date}}{date and time of recording}
+#'   \item{\code{prompt}}{sentence stimulus}
+#'   \item{\code{label}}{\code{AAA} annotation label}
+#'   \item{\code{TT_displacement_sm}}{smoothed Tongue Tip displacement}
+#'   \item{\code{TT_velocity}}{Tongue Tip velocity}
+#'   \item{\code{TT_velocity_abs}}{absolute Tongue Tip velocity}
+#'   \item{\code{TD_displacement_sm}}{smoothed Tongue Dorsum displacement}
+#'   \item{\code{TD_velocity}}{Tongue Dorsum velocity}
+#'   \item{\code{TD_velocity_abs}}{absolute Tongue Dorsum velocity}
+#'   \item{\code{TR_displacement_sm}}{smoothed Tongue Root displacement}
+#'   \item{\code{TR_velocity}}{Tongue Root velocity}
+#'   \item{\code{TR_velocity_abs}}{Absolute Tongue Root velocity}
+#'   \item{\code{fan_line}}{number of fan line}
+#'   \item{\code{X}}{x coordinate}
+#'   \item{\code{Y}}{y coordinate}
+#'   \item{\code{word}}{word stimulus}
+#'   \item{\code{language}}{speaker's native language (\code{Italian}, \code{Polish})}
+#'   \item{\code{sex}}{speaker's sex (\code{f}, \code{m})}
+#'   \item{\code{item}}{word ID number}
+#'   \item{\code{ipa}}{IPA transcription of the word}
+#'   \item{\code{c1}}{first consonant (C1)}
+#'   \item{\code{c1_phonation}}{voicing of C1 (\code{voiceless}, \code{voiced})}
+#'   \item{\code{vowel}}{V1 and V2 (\code{a}, \code{o}, \code{u})}
+#'   \item{\code{anteropost}}{backness of the vowel (\code{back}, \code{central})}
+#'   \item{\code{height}}{height of the vowel (\code{high}, \code{mid}, \code{low})}
+#'   \item{\code{c2}}{second consonant (C2)}
+#'   \item{\code{c2_phonation}}{voicing of C2 (\code{voiceless} or \code{voiced})}
+#'   \item{\code{c2_place}}{place of C2 (\code{coronal}, \code{velar})}
+#'}
+"tongue_contours"
+
+#' Tracegram data of Italian and Polish vowels
+#'
+#' First derivative maximum and minimum trakectories of the electroglottographic
+#' data during the production of vowels in Italian and Polish.
+#'
+#' @format A tibble with 16754 observation and 23 variables:
+#' \describe{
+#'   \item{\code{speaker}}{speaker's ID}
+#'   \item{\code{file}}{audio chunk file name}
+#'   \item{\code{date}}{date and time of recording}
+#'   \item{\code{word}}{word stimulus}
+#'   \item{\code{time}}{(seconds) time within audio chunk file}
+#'   \item{\code{rel.time}}{(seconds) time from Vowel onset}
+#'   \item{\code{proportion}}{proportion of vowel duration}
+#'   \item{\code{maximum}}{dEGG maximum}
+#'   \item{\code{minimum}}{dEGG minimum}
+#'   \item{\code{closed_quotient}}{close quotient}
+#'   \item{\code{peaks_ratio}}{peaks ratio}
+#'   \item{\code{language}}{speaker's native language (\code{Italian}, \code{Polish})}
+#'   \item{\code{sex}}{speaker's sex (\code{f}, \code{m})}
+#'   \item{\code{item}}{word ID number}
+#'   \item{\code{ipa}}{IPA transcription of the word}
+#'   \item{\code{c1}}{first consonant (C1)}
+#'   \item{\code{c1_phonation}}{voicing of C1 (\code{voiceless}, \code{voiced})}
+#'   \item{\code{vowel}}{V1 and V2 (\code{a}, \code{o}, \code{u})}
+#'   \item{\code{anteropost}}{backness of the vowel (\code{back}, \code{central})}
+#'   \item{\code{height}}{height of the vowel (\code{high}, \code{mid}, \code{low})}
+#'   \item{\code{c2}}{second consonant (C2)}
+#'   \item{\code{c2_phonation}}{voicing of C2 (\code{voiceless} or \code{voiced})}
+#'   \item{\code{c2_place}}{place of C2 (\code{coronal}, \code{velar})}
+#'}
+"tracegram"
+
+#' Wavegram data of Italian and Polish vowels
+#'
+#' First derivative amplitude of the electroglottographic signal during the production
+#' of vowels in Italian and Polish.
+#'
+#' @format A data frame with 2212533 rows and 21 variables:
+#' \describe{
+#'   \item{\code{speaker}}{speaker's ID}
+#'   \item{\code{file}}{audio chunk file name}
+#'   \item{\code{date}}{date and time of recording}
+#'   \item{\code{word}}{word stimulus}
+#'   \item{\code{rel_time}}{(seconds) time from Vowel onset}
+#'   \item{\code{time}}{(seconds) time within audio chunk file}
+#'   \item{\code{sequence}}{index of glottal cycle within vowel duration}
+#'   \item{\code{sample}}{sample within glottal cycle}
+#'   \item{\code{amplitude}}{relative amplitude of the electroglottographic signal}
+#'   \item{\code{language}}{speaker's native language (\code{Italian}, \code{Polish})}
+#'   \item{\code{sex}}{speaker's sex (\code{f}, \code{m})}
+#'   \item{\code{item}}{word ID number}
+#'   \item{\code{ipa}}{IPA transcription of the word}
+#'   \item{\code{c1}}{first consonant (C1)}
+#'   \item{\code{c1_phonation}}{voicing of C1 (\code{voiceless}, \code{voiced})}
+#'   \item{\code{vowel}}{V1 and V2 (\code{a}, \code{o}, \code{u})}
+#'   \item{\code{anteropost}}{backness of the vowel (\code{back}, \code{central})}
+#'   \item{\code{height}}{height of the vowel (\code{high}, \code{mid}, \code{low})}
+#'   \item{\code{c2}}{second consonant (C2)}
+#'   \item{\code{c2_phonation}}{voicing of C2 (\code{voiceless} or \code{voiced})}
+#'   \item{\code{c2_place}}{place of C2 (\code{coronal}, \code{velar})}
+#'}
+"wavegram"
