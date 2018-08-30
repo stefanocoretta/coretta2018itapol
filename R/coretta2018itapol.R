fda49c168ae0832b3a9bc0d2af1e0ec74e8ec640
Stefano Coretta <stefanoc.social@me.com>
2018-08-30 17:02:25 +0200

    add package doc

---
+++ R/coretta2018itapol.R
@@ -0,0 +1,11 @@
+#' coretta2018itapol: Data package of an acoustic and articulatory study of the
+#' voicing effect in Italian and Polish
+#'
+#' This package provides datasets containing variuos acoustic and articulatory
+#' measurements of Italian and Polish. The data are part of an exploratory study
+#' on the effect of consonant voicing on preceding vowel duration (the voicing
+#' effect).
+#'
+#' @docType package
+#' @name coretta2018itapol
+NULL
